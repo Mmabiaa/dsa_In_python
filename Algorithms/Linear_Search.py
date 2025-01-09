d3f9bdaa6c70835160662b686f8844f129dc03df
Boateng Prince Agyenim <163312213+Mmabiaa@users.noreply.github.com>
2025-01-09 18:42:20 +0000

    Delete Algorithms directory

--- Algorithms/Linear_Search.py
+++
@@ -1,8 +0,0 @@
-def linear_search(arr, target):
-    # Traverse through the array
-    for index, value in enumerate(arr):
-        # Check if the current element matches the target
-        if value == target:
-            return index  # Return the index if found
-    
-    return -1  # Return -1 if the target is not found in the array
